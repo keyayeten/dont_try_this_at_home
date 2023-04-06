98cc68139a74e71a266537437f0fb4b57f0b43be
keyayeten <76627566+keyayeten@users.noreply.github.com>
2023-04-06 18:02:39 +0300

    reviwed

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,12 +6,7 @@ from django.shortcuts import get_object_or_404
 
 
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к
-    # меньшим)
     posts = Post.objects.order_by('-pub_date')[:10]
-    # В словаре context отправляем информацию в шаблон
     context = {
         'posts': posts,
     }
@@ -19,16 +14,10 @@ def index(request):
 
 
 def group_posts(request, slug):
-    # Функция get_object_or_404 получает по заданным критериям объект
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
+    num_of_posts = 10
     group = get_object_or_404(Group, slug=slug)
-
-    # Метод .filter позволяет ограничить поиск по критериям.
-    # Это аналог добавления
-    # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    related_name = group
+    posts = Post.objects.filter(group=related_name).order_by('-pub_date')[:num_of_posts]
     context = {
         'group': group,
         'posts': posts,
