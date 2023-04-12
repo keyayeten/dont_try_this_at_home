9749479692fcce290e598e65e98aeea9065b5e46
keyayeten <76627566+keyayeten@users.noreply.github.com>
2023-04-12 23:39:38 +0300

    added registration form and related models

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -20,4 +20,6 @@ from django.urls import include
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
