60f53f692ce8fb31e7e06e7d1888906eb31169d0
keyayeten <76627566+keyayeten@users.noreply.github.com>
2023-04-16 17:41:13 +0300

    added user post and post_details + pagination

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post, Group
+from .models import Post, Group, User
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
 
@@ -35,3 +35,33 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+
+def profile(request, username):
+    # Здесь код запроса к модели и создание словаря контекста
+    user_id = User.objects.get(username=username).id
+    user_posts = Post.objects.all().filter(author=user_id)
+
+    paginator = Paginator(user_posts, NUM_OF_POSTS)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'name': username,
+        'page_obj': page_obj,
+        'post_count': len(user_posts),
+        'id': user_id,
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    # Здесь код запроса к модели и создание словаря контекста
+    post = Post.objects.get(pk=post_id)
+    user_id = User.objects.get(username=post.author).id
+    user_posts = Post.objects.all().filter(author=user_id)
+
+    context = {
+        'post': post,
+        'count': len(user_posts)
+    }
+    return render(request, 'posts/post_detail.html', context)
